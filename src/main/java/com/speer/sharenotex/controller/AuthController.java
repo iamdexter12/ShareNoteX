ff89d54c773bd4afeaaeaac0fbd6a48ed2b88868
Md Sharif <md.sharif@nagarro.com>
2024-02-21 19:13:52 +0530

    Added package

--- src/main/java/com/speer/sharenotex/controller/AuthController.java
+++ src/main/java/com/speer/sharenotex/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import com.speer.sharenotex.config.JwtService;
 import com.speer.sharenotex.exception.NotFoundException;
 import com.speer.sharenotex.requesdto.LoginRequestDto;
